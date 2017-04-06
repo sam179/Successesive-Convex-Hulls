0a76a5a3182b7318b1201ab086d105a8f3fda9f8
shikha holker <shikhaholker@dv17.local>
2017-04-06 12:34:53 +0530

    Quick hull implemented and works in conjunction with Jarvis March algo. Code works perfectly fine without any bug found. Project is complete but the GUI part.

--- assignment2.py
+++ assignment2.py
@@ -22,8 +22,129 @@ def func(Points,a):
 			return idx
 
 		idx = idx + 1
+###############################################################################################################################################
+## Quick Hull recursive algorithm		
+###############################################################################################################################################
+def isCCW(a,x,b):
+
+    #print b
+    val = ((x[1]-a[1])*(b[0]-x[0]) - (x[0]-a[0])*(b[1]-x[1]))
+    #print val
+    if val==0:
+        return 0
+    elif val>0:
+        return 1
+    else:
+        return 2
+
+def get_hull_points(listPts):
+
+    # get the min, and max from the list of points
+    min, max = get_min_max_x(listPts)
+
+    hullpts = quickhull(listPts, min, max)
+
+    hullpts = hullpts + quickhull(listPts, max, min)
+
+    return hullpts 
+
+'''
+    Does the sorting for the quick hull sorting algorithm
+'''
+def quickhull(listPts, min, max):
+    left_of_line_pts = get_points_left_of_line(min, max, listPts)
+
+    ptC = point_max_from_line(min, max, left_of_line_pts)
+
+    if len(ptC) < 1:
+        return [max]
+
+    hullPts = quickhull(left_of_line_pts, min, ptC)
+
+    hullPts = hullPts + quickhull(left_of_line_pts, ptC, max)
+
+    return hullPts
+
+'''
+    Reterns all points that a LEFT of a line start->end
+'''
+def get_points_left_of_line(start, end, listPts):
+    pts = []
+
+    for pt in listPts:
+        if isCCW(start, end, pt)==2:
+            pts.append(pt)
+
+    return pts
+
+'''
+    Returns the maximum point from a line start->end
+'''
+def point_max_from_line(start, end, points):
+    max_dist = 0
+
+    max_point = []
+
+    for point in points:
+        if (point[0]!=start[0] or point[1]!=start[1]) and (point[1]!=end[1] or point[0]!=end[0]):
+            dist = distance(start, end, point)
+            if dist > max_dist:
+                max_dist = dist
+                max_point = point
+
+    return max_point
+
+def get_min_max_x(list_pts):
+    min_x = float('inf')
+    max_x = 0
+    min_y = 0
+    max_y = 0
+
+    for x,y in list_pts:
+        if x < min_x:
+            min_x = x
+            min_y = y
+        if x > max_x:
+            max_x = x
+            max_y = y
+
+    return [min_x,min_y], [max_x,max_y]
+
+'''
+    Given a line of start->end, will return the distance that
+    point, pt, is from the line.
+'''
+def distance(start, end, pt): # pt is the point
+    x1, y1 = start
+    x2, y2 = end
+    x0, y0 = pt
+    nom = abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1)
+    denom = ((y2 - y1)**2 + (x2 - x1) ** 2) ** 0.5
+    result = nom / denom
+    return result
 
 
+def caller_func(Points):
+
+	if len(Points)<2:
+
+		return
+
+	Current_Hull = get_hull_points(Points)
+
+	print "yo"
+	for x in Current_Hull: print x
+
+	temp_list = []
+
+	temp_list = [x for x in Points if x not in Current_Hull]
+
+	caller_func(temp_list)
+
+	return
+#############################################################################################################################################
+## Gift Wrapping Algorithm
+#############################################################################################################################################
 def Gift_Wrapping(Points,n):
 
 	min_idx = np.argmin(Points,axis=0)
@@ -53,13 +174,29 @@ def Gift_Wrapping(Points,n):
 			break
 
 	Hull = np.delete(Hull,0)
+
+
+
 	Points = np.delete(Points,Hull,0)
 	
 	for x in Hull: print x
 
+	#print "yo"
+	
+	#print Points
 	for x in Points:
 
-		print x
+		x.tolist()
+
+	#print Points	
+
+	Points.tolist()
+
+	#print type(Points)
+	#print type(Points[0])
+	caller_func(Points.tolist())
+
+	return
 
 
 def main():
@@ -67,9 +204,10 @@ def main():
 	#Entering no. of points
 	n = input("Enter the value for n: ")
 
-	Points = np.random.randint(10,size=(n,2))
-
+	Points = np.random.randint(100,size=(n,2))
+	
 	#sorting points
+	
 	Points[Points[:0].argsort()]
 
 	for p in Points:
